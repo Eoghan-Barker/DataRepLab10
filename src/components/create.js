e45dcbaff7dfb3a139380df2c39573084405764e
Eoghan-Barker <g00397072@gmit.ie>
2022-10-18 12:20:47 +0100

    lab complete

--- src/components/create.js
+++ src/components/create.js
@@ -1,11 +1,78 @@
-import React from 'react';
+import React from "react";
 
 export class Create extends React.Component {
-    render() {
-        return (
-            <div>
-                <h3>Hello from my Create Component</h3>
-            </div>
-        );
-    }
+  constructor() {
+    super();
+    // Binding event is needed in constructor to handle event invoking
+    this.handleSubmit = this.handleSubmit.bind(this);
+    this.onChangeBookTitle = this.onChangeBookTitle.bind(this);
+    this.onChangeBookAuthor = this.onChangeBookAuthor.bind(this);
+    this.onChangeBookCover = this.onChangeBookCover.bind(this);
+    this.state = {
+      title: '',
+      author: '',
+      cover: ''
+    };
+  }
+  handleSubmit(e) {
+    e.preventDefault();
+    console.log(`${this.state.title},
+    ${this.state.author}, ${this.state.cover}`);
+    // clear the state after logging
+    this.setState({title:'', author:'', cover:''});
+  }
+
+  // Add the inputed values to the state
+  onChangeBookTitle(e) {
+    this.setState({ title: e.target.value });
+  }
+
+  onChangeBookAuthor(e) {
+    this.setState({ author: e.target.value });
+  }
+
+  onChangeBookCover(e) {
+    this.setState({ cover: e.target.value });
+  }
+
+  render() {
+    return (
+      <div>
+        <h3>Hello from my Create Component</h3>
+        {/* React form acts similarly to html one, uses JSX
+        Invoke methods on submit button press and when the input value is changed
+        to update the state */}
+        <form onSubmit={this.handleSubmit}>
+          <div className="form-group">
+            <label>Add Book Title: </label>
+            <input
+              type="text"
+              className="form-control"
+              value={this.state.title}
+              onChange={this.onChangeBookTitle}
+            />
+          </div>
+          <div className="form-group">
+            <label>Add Book Author: </label>
+            <input
+              type="text"
+              className="form-control"
+              value={this.state.author}
+              onChange={this.onChangeBookAuthor}
+            />
+          </div>
+          <div className="form-group">
+            <label>Add Book Cover: </label>
+            <input
+              type="text"
+              className="form-control"
+              value={this.state.cover}
+              onChange={this.onChangeBookCover}
+            />
+          </div>
+          <input type="submit" value="Submit" />
+        </form>
+      </div>
+    );
+  }
 }
--- src/components/read.js
+++ src/components/read.js
@@ -1,44 +1,27 @@
 import React from 'react';
 import { Books } from './books';
+import axios from 'axios';
 
 export class Read extends React.Component {
     // state is used to pass items down to children using state.props.xxx <= json data headers
+    // make http request using httpclient axios to pull json data
+    // axios makes a http request to the api
+    componentDidMount() {
+        // promise -result of an async operation
+        axios.get('https://jsonblob.com/api/jsonblob/1027219693823606784')
+        //callback function - a function is passed as an argument to the function
+        .then((response)=> {
+            this.setState({
+               books: response.data 
+            })
+            // this function is fulfilling the promise
+        })
+        .catch((error)=>{console.log(error)});   // handles rejected path
+    }
+    
     state = {
         // Book Array of book objects
-        books: [
-            {
-            "title": "Learn Git in a Month of Lunches",
-            "isbn": "1617292419",
-            "pageCount": 0,
-            "thumbnailUrl": "https://s3.amazonaws.com/AKIAJC5RLADLUMVRPFDQ.book-thumb-images/umali.jpg",
-            "status": "MEAP",
-            "authors": ["Rick Umali"],
-            "categories": []
-            },
-            {
-            "title": "MongoDB in Action, Second Edition",
-            "isbn": "1617291609",
-            "pageCount": 0,
-            "thumbnailUrl": "https://s3.amazonaws.com/AKIAJC5RLADLUMVRPFDQ.book-thumb-images/banker2.jpg",
-            "status": "MEAP",
-            "authors": [
-            "Kyle Banker",
-            "Peter Bakkum",
-            "Tim Hawkins",
-            "Shaun Verch",
-            "Douglas Garrett"
-            ],
-            "categories": []
-            },
-            {
-            "title": "Getting MEAN with Mongo, Express, Angular, and Node",
-            "isbn": "1617292036",
-            "pageCount": 0,
-            "thumbnailUrl": "https://s3.amazonaws.com/AKIAJC5RLADLUMVRPFDQ.book-thumb-images/sholmes.jpg",
-            "status": "MEAP",
-            "authors": ["Simon Holmes"],
-            "categories": []
-            }]
+        books: [ ]
     }
     render(){
         return(
